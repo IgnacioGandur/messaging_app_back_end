460015adcf8abd18247d172296cf12e15a747ee1
Ignacio Gandur <gandurjuanignacio@gmail.com>
2026-04-08 11:26:08 -0300

    Fix: Updated prisma schema path in config file.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-    schema: "prisma/prisma.schema",
+    schema: "prisma/schema.prisma",
     datasource: {
         url: String(process.env.DATABASE_URL),
     },
